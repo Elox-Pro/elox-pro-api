0b273013f41c3a5efd5afa15cf3530b37c6e4f3d
YonatanQuintero <yonatan.a.quintero.r@gmail.com>
2024-05-17 07:55:59 -0500

    Testing for update gender is done!

--- test/e2e/profile/update-gender.uc.e2e-spec.ts
+++ test/e2e/profile/update-gender.uc.e2e-spec.ts
@@ -4,58 +4,39 @@ import { AuthenticationModule } from "@app/authentication/authentication.module"
 import { UserModule } from "@app/user/user.module";
 import { createJwtCookieSession } from "../test-helpers/create-jwt-cookie-session.test-helper";
 import * as request from "supertest";
-import { UpdateNameRequestDto } from "@app/user/dtos/update-name/update-name.request.dto";
-import { UpdateNameResponseDto } from "@app/user/dtos/update-name/update-name.responde.dto";
 import { UpdateGenderRequestDto } from "@app/user/dtos/update-gender/update-gender.request.dto";
 import { Gender } from "@prisma/client";
 import { UpdateGenderResponseDto } from "@app/user/dtos/update-gender/update-gender.response.dto";
+import { getTestUser } from "../test-helpers/get-test-user.test-helper";
+import { CreateRequestFN, createPatch } from "../test-helpers/create-request.test-helper";
 
 describe("Update gender use case", () => {
+    const url = "/users/profile/gender";
+    const user = getTestUser();
+
     let app: INestApplication;
+    let patch: CreateRequestFN;
 
     beforeAll(async () => {
         app = await bootstrapTest([
             AuthenticationModule,
             UserModule
         ]);
+        patch = createPatch({
+            app, url, credentials: {
+                username: user.username,
+                password: user.password
+            }
+        });
     });
 
     afterAll(async () => {
         await app.close();
     });
-
     describe("PATCH: users/profile/gender", () => {
-        const username = "alaska";
-        const password = "098lkj!";
-        const url = "/users/profile/gender";
-
-        let cookies: string;
-
-        it("should authenticate the user", async () => {
-            cookies = await createJwtCookieSession(
-                app.getHttpServer(),
-                username,
-                password
-            );
-
-            expect(cookies).toBeDefined();
-        });
-
         it("should return HTTP status OK", async () => {
-
-            const res = await request(app.getHttpServer())
-                .patch(url)
-                .set('Cookie', cookies)
-                .send({
-                    gender: Gender.MALE,
-                } as UpdateGenderRequestDto);
-
+            const res = await patch({ gender: Gender.MALE });
             expect(res.status).toBe(HttpStatus.OK);
-            expect(res.body).toBeDefined();
-            const body = res.body as UpdateGenderResponseDto;
-            expect(body.OK).toBe(true);
-            expect(HttpStatus.OK).toBe(res.status);
         });
-
     });
 });
